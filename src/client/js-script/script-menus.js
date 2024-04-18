a390e70dd33e6382bde97384cfbcf8425c7db230
Said Mazaheri <ysmazaheri@gmail.com>
2024-04-18 18:30:47 -0400

    Menu search bar now filters the results via 'contains'

--- src/client/js-script/script-menus.js
+++ src/client/js-script/script-menus.js
@@ -1,9 +1,27 @@
 const searchBarElement = document.getElementById('search-bar');
+let foodOptions = document.getElementsByClassName('food-option');
 
-function loadMenu() {
+// Refine search every time the input value changes
+searchBarElement.addEventListener('input', filterOptions);
 
-
-
+function filterOptions() {
+    // Update the list of what food options are on the page currently, in case it has changed
+    foodOptions = document.getElementsByClassName('food-option');
+    let foodOptionsArr = [].slice.call(foodOptions);
+    // Get the search query
+    let query = String(searchBarElement.value).toLowerCase();
+    // Filter via contains
+    foodOptionsArr.forEach(foodOption => {
+        // Food options are list items. The contents are children HTML elements, buttons
+        let foodLabel = foodOption.children[0];
+        let foodName = String(foodLabel.value).toLowerCase();
+        // Remove/show items by toggling 'hiding'
+        if (foodName.includes(query)) {
+            // Show if matches query
+            foodOption.style.display = 'list-item';
+        } else {
+            // Show if matches query
+            foodOption.style.display = 'none';
+        }
+    });
 }
-
-loadMenu();
