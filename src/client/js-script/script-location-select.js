aaaac22c737ea8349126a4b6620cfb5488e3089c
Said Mazaheri <ysmazaheri@gmail.com>
2024-04-18 13:59:18 -0400

    Back Buttons & File Restructure

--- src/client/js-script/script-location-select.js
+++ src/client/js-script/script-location-select.js
@@ -2,3 +2,8 @@ document.getElementById('nextBtn').addEventListener('click', function() {
     // Navigate to order.html
     window.location.href = 'order.html';
   });
+
+  document.getElementById('backBtn').addEventListener('click', function() {
+    // Navigate to home.html
+    window.location.href = 'home.html';
+  });
