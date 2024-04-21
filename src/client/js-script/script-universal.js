f94f346ccd9f12a20fbe069457c9eeaf5cf08721
Said Mazaheri <ysmazaheri@gmail.com>
2024-04-21 12:28:08 -0400

    Set minimum dimensions for universal. Fix scaling

--- src/client/js-script/script-universal.js
+++ src/client/js-script/script-universal.js
@@ -5,17 +5,21 @@ toggleButton.addEventListener("click", toggleSidebar);
 
 function toggleSidebar() {
     let sidebar = document.getElementsByClassName("sidebar").item(0);
+    let topbar = document.getElementsByClassName("topbar").item(0);
     let main = document.getElementsByClassName("main").item(0);
+    
     // Hide sidebar
     if (sidebar.style.width !== "0px") {
         sidebar.style.width = "0px";
         sidebar.style.display = "none";
         main.style.width = "100vw";
+        topbar.style.minWidth = "835px"
     }
     // Show sidebar
     else {
         sidebar.style.width = "165px";
         sidebar.style.display = "block";
         main.style.width = "calc(100vw - 165px)";
+        topbar.style.minWidth = "1000px"
     }
 }
