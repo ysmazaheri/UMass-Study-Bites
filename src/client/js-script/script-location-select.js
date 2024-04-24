3443d1179fbf850c69233a5ef0ea0a8423578ec6
Said Mazaheri <ysmazaheri@gmail.com>
2024-04-24 11:42:15 -0400

    Temporarily hide API key in .js file

--- src/client/js-script/script-location-select.js
+++ src/client/js-script/script-location-select.js
@@ -1,3 +1,5 @@
+import { mapKey } from '../secrets.js'
+
 // UI components
 const searchBarElement = document.getElementById('search-bar-text');
 let map;
@@ -227,7 +229,7 @@ searchBarElement.addEventListener('input', (query) => {
 async function initMap() {
     // Initialize map documentation
     (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-          key: "AIzaSyBiRzlT6JAWBzZG_cvbHahzoLIMU5THjIw",
+          key: mapKey(),
           v: "weekly",
         });
     // Request libraries when needed, not in the script tag.
