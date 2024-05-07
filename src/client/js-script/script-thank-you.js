3fb47aaf8caf34490436ee97193a1bcea80cabac
ianmcgregor1 <imcgregor@umass.edu>
2024-05-07 16:22:13 -0400

    moved html files out of html-pages and renamed home.html to index.html

--- src/client/js-script/script-thank-you.js
+++ src/client/js-script/script-thank-you.js
@@ -1,6 +1,6 @@
 // Hardcoded back button
 const backButton = document.getElementById('back-to-home');
 backButton.addEventListener("click", () => {
-    // Change window to home.html
-    window.location.href = "home.html";
+    // Change window to index.html
+    window.location.href = "index.html";
 })
