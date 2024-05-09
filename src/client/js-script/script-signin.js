621a0b969e41d2705a5f4799c7c88aa6e73f7f1b
Vrishabhshri <vrishabhshri@gmail.com>
2024-05-09 16:45:32 -0400

    Added new js

--- src/client/js-script/script-signin.js
+++ src/client/js-script/script-signin.js
@@ -1,26 +1,48 @@
+import User from "../js-models/user.js";
+
+
+/**
+ * DOM elements for cover section
+ */
 const coverButton = document.getElementById('sign-in-sign-up-cover-button');
 const coverBox = document.getElementById('sign-in-sign-up-cover');
 const coverText = document.getElementById('sign-in-sign-up-cover-text');
 const coverTitle = document.getElementById('sign-in-sign-up-cover-title');
 const coverDesc = document.getElementById('sign-in-sign-up-cover-desc');
 
+/**
+ * DOM elements for form section
+ */
 const formButton = document.getElementById('sign-in-sign-up-form-button');
 const formBox = document.getElementById('sign-in-sign-up-form');
+const formLogin = document.getElementById('sign-in-sign-up-form-fillout-login');
+const formRegister = document.getElementById('sign-in-sign-up-form-fillout-register');
 const formTitle = document.getElementById('sign-in-sign-up-form-title');
-
 const confirmPassInput = document.getElementById('confirm-pass');
 
+/**
+ * Text content for cover section
+ */
 const titles = ["Sign In", "Sign Up"];
 const coverTitles = ["New here?",
                     "Sign In"];
 const coverDescs = ["Sign up for fast, safe, and efficient delivery services. Open to all UMass students and faculty.",
                     "Sign in to start ordering today!"];
 const coverButtonValues = ["Sign Up",
-                            "Sign In"]
+                            "Sign In"];
+const formType = ["none", "flex"];
 
-// For getting info from forms, idxCount 0 = Sign in info, idxCount = 1 Sign up info
+formLogin.style.display = formType[1];
+formRegister.style.display = formType[0];
+
+/**
+ * Index count for switching between sign in and sign up
+ */
 let idxCount = 0;
 
+/**
+ * Slide the cover and form sections to switch between sign in and sign up
+ */
 function slide () {
     idxCount = (idxCount + 1) % 2;
     // Transition to fade cover text
@@ -37,6 +59,10 @@ function slide () {
     setTimeout(() => { 
         // Changin form text
         formTitle.innerHTML = titles[idxCount];
+        formButton.innerHTML = coverButtonValues[(idxCount + 1) % 2];
+        formRegister.style.display = formType[idxCount];
+        formLogin.style.display = formType[(idxCount + 1) % 2];
+
         // Adding confirm password input field
         if (idxCount) confirmPassInput.removeAttribute("hidden");
         else confirmPassInput.setAttribute("hidden", "");
@@ -45,7 +71,7 @@ function slide () {
     setTimeout(() => {
         coverTitle.innerHTML = coverTitles[idxCount];
         coverDesc.innerHTML = coverDescs[idxCount];
-        coverButton.value = coverButtonValues[idxCount];
+        coverButton.innerHTML = coverButtonValues[idxCount];
         // Fade in new button and text
         coverText.classList.toggle('fadeTextAndButton');
         coverButton.classList.toggle('fadeTextAndButton');
@@ -54,27 +80,64 @@ function slide () {
 
 coverButton.addEventListener("click", () => slide());
 
-// User sign in check
-const tokenBox = document.getElementsByClassName('token-container')[0];
-tokenBox.style.display = 'none';
+const ls = window.localStorage;
 
-const URL = "http://localhost:3000";
+/**
+ * Handle form submission for sign in or sign up
+ */
+formButton.addEventListener("click", async () => {
+    if (idxCount === 1) {
+        // Sign up
+        try {
+            const pass = document.getElementById('pass').value;
+            const user = document.getElementById('user').value;
+            const confpass = document.getElementById('confirm-pass').value;
 
-// TODO: Implement
-async function checkSignedIn() {
+            if (pass !== confpass) {
+                alert("Passwords must match.");
+            }
 
-    // Check if user signed in
-    try{
-        let loginResponse = await fetch(`${URL}/login`, {
-        method: "GET",
-        });
-        let loginJSON = await loginResponse.json();
-        let username = loginJSON.__id;
-        console.log(username);
-    }catch(ex){
-        console.log('Error retrieving username');
+            const userNew = new User(user, pass);
+            //fetch user route from server.js. routes are not done.
+            localStorage.setItem('currentUser', JSON.stringify(userNew));
+            alert("Sign up successful.");
+        } catch (e) {
+            handleError(e);
+        }
+
+        document.getElementById('pass').value = '';
+        document.getElementById('user').value = '';
+        document.getElementById('confirm-pass').value = '';
+    } else {
+        // Sign in
+        try {
+            const pass = document.getElementById('pass');
+            const user = document.getElementById('user');
+            const loadedUser = await loadUser(user); 
+          
+            if (loadedUser.password !== pass || loadedUser === undefined) {
+                alert("Incorrect Username or Password");
+                return;
+            }
+            
+            localStorage.setItem('currentUser', JSON.stringify(loadedUser));
+            tokenBox.style.display = 'display';
+            alert("Sign in successful.");
+        } catch (e) {
+            handleError(e);
+        }
+
+        document.getElementById('pass').value = '';
+        document.getElementById('user').value = '';
     }
+})
 
+/**
+ * Handle errors during sign in or sign up
+ * @param {Error} error - The error object
+ */
+function handleError(error) {
+    console.error(error);
+    alert("Incorrect Sign-in Attempt. Please try again.");
 }
 
-checkSignedIn();
